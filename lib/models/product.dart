79fa53db2f6c1e421755451575b11a497a192976
Barend Koorzen <koorzenb@gmail.com>
2022-10-04 07:53:19 +0200

    Working on the "Products" Grid & Item Widgets

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -7,14 +7,14 @@ class Product {
   final String description;
   final double price;
   final String imageUrl;
-  bool isFavourite = false;
+  bool isFavourite;
 
-  Product(
-    @required this.id,
-    @required this.title,
-    @required this.description,
-    @required this.price,
-    @required this.imageUrl,
-    this.isFavourite,
-  );
+  Product({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    this.isFavourite = false,
+  });
 }
