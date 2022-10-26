58f3d7189ddbbe5fa86223f8f8ad4519da9915c4
Barend Koorzen <koorzenb@gmail.com>
2022-10-26 07:51:31 +0200

    Providing an Orders Object

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/orders.dart';
 import 'screens/cart_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -19,6 +20,9 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: (ctx) => Cart(),
+        ),
+        ChangeNotifierProvider(
+          create: (ctx) => Orders(),
         ),
       ],
       child: MaterialApp(
--- test/providers/product_test.dart
+++ test/providers/product_test.dart
@@ -7,4 +7,4 @@ void main() {
     expect(isTrue, true);
   });
 }
-    // Arrange
+// Arrange
