58dc8d28241cab560bda550272b90171bb7c56ee
Barend Koorzen <koorzenb@gmail.com>
2022-10-28 04:59:46 +0200

    "Edit Product" Screen

---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class UserProductScreen extends StatefulWidget {
+  const UserProductScreen({Key key}) : super(key: key);
+
+  @override
+  State<UserProductScreen> createState() => _UserProductScreenState();
+}
+
+class _UserProductScreenState extends State<UserProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Product'),
+      ),
+      body: ,
+    );
+  }
+}
